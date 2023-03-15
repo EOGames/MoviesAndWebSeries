6a577987f6777662be87d5a0bc46cd3f227275fa
EOGames <113043641+EOGames@users.noreply.github.com>
2023-03-15 21:04:02 +0530

    Serchbar position adjusted

--- src/SerchBar.js
+++ src/SerchBar.js
@@ -3,7 +3,7 @@ import React from "react";
 const SerchBar = function(props)
 {
     return(
-        <div>
+        <div className="srchHolder">
             <input id="serchbar" type={'search'} placeholder ={'enter an name to serch'}  onChange = {props.searchChange} />
         </div>
     );
